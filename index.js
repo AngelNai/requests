ab160581e317014fe07e977fb7ba52ac6504809d
Angel Armando Gonzalez Rodriguez <kuategonz@gmail.com>
2023-09-20 13:42:50 -0600

    Query params

--- index.js
+++ index.js
@@ -14,6 +14,21 @@ app.get('/:user/:password',(req, res)=>{
     res.status(404).json({msg:'error en el usuario o contraseña'})
 })
 
+//http://localhost:3000/login?user=angel&password=123
+app.get('/login',(req,res)=>{ //endpoint
+const queries=req.query
+if(!user || !password){
+    res.status(400).json({msg:'uou need to provide <user> and <password> parameters'})
+    return
+}
+if  (user ==='angel' && password=== '123'){
+    res.json({msg: 'inicio de sesion exitoso'})
+    return
+}
+res.json(queries)
+})
+
+
 app.post('/',(req,res)=>{res.json({msg:'hello post'})})
 app.put('/',(req,res)=>{res.json({msg:'hello put'})})
 app.delete('/',(req,res)=>{res.json({msg:'hello delete'})})
